0456167a085a809539426c91cfcdaa27ba00c49a
Giovanni Pecorelli <48129769+GioPec@users.noreply.github.com>
2020-08-08 19:08:20 +0200

    facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, '930685917439032', 'fbd419015abb165e0578c10dfd80cef0', scope: 'name, email, user_gender, user_birthday, user_photos'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_08_080257) do
+ActiveRecord::Schema.define(version: 2020_08_08_155955) do
 
   create_table "recipes", force: :cascade do |t|
     t.integer "user_id"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2020_08_08_080257) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username", limit: 20, default: "", null: false
+    t.string "username", limit: 20, default: " ", null: false
     t.string "first_name", limit: 20, default: " ", null: false
     t.string "last_name", limit: 20, default: " ", null: false
     t.date "date_of_birth"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2020_08_08_080257) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
