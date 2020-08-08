9ca992710b47af90b5b18df44c9da0f8a47a676b
Giovanni Pecorelli <48129769+GioPec@users.noreply.github.com>
2020-08-08 12:44:14 +0200

    login signup profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
 
     def index
-        render html: 'index!'
+        @users = User.all
     end
+
+    def show
+        id = params[:id]
+        if User.exists?(id)
+            @user = User.find(id)
+        else
+            render html: 'User does not exit'   #TODO 404
+        end
+	end
 end
