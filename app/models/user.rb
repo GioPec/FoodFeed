04a368b59a96ced62a7354cd4a5d13fe56db2f14
Giovanni Pecorelli <48129769+GioPec@users.noreply.github.com>
2020-08-11 17:28:34 +0200

    comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
 
     has_many :recipes
     has_many :likes
+    has_many :comments
 
     def self.from_omniauth(auth) 
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
