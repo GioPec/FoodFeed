86acea1173b82517c1b626698ec6655369628b8a
JacopoRossi <64418613+JacopoRossi@users.noreply.github.com>
2020-08-26 12:44:58 +0200

    db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-    a = User.create(username: "admin", email: "a@a.a", password: "aaaaaaaa", role: "A", first_name: "admin", last_name: "admin")
+    a = User.create(username: "admin", email: "admin@mail.it", password: "adminadmin", role: "A", first_name: "admin", last_name: "admin")
     a.add_role :admin
     a.add_role :admin, Comment
     a.add_role :admin, User
@@ -16,3 +16,13 @@
     a.add_role :mod, User
     a.add_role :mod, Recipe
     a.save
+
+    m = User.create(username: "moderator", email: "moderator@mail.it", password: "moderator", role: "M", first_name: "moderator", last_name: "moderator")
+    m.add_role :mod
+    m.add_role :mod, Comment
+    m.add_role :mod, User
+    m.add_role :mod, Recipe
+    m.save
+
+    u = User.create(username: "user", email: "user@mail.it", password: "useruser", role: "U", first_name: "user", last_name: "user")
+    
